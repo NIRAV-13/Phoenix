f80d5890b706be8b229a6ca677130485d25d3031
System Administrator <root@T99B5.WPA.Dal.Ca>
2022-04-04 13:57:30 -0300

    Admin

--- PH/src/main/java/proj/springboot/web/MainController.java
+++ PH/src/main/java/proj/springboot/web/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import proj.springboot.admin.admin_details;
 import proj.springboot.parking.Parking_details;
 import proj.springboot.parking.booking_details;
 import proj.springboot.parking.reading_database;
@@ -19,8 +20,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static proj.springboot.Map.direction.direction_finder;
+import static proj.springboot.admin.admin_cards.admin_cards;
 import static proj.springboot.parking.Booking_slots.payment_cards;
 import static proj.springboot.parking.reading_database.parking_cards;
+import static proj.springboot.service.UserServiceImpl.email_123;
 
 @Controller
 public class MainController {
@@ -70,27 +73,28 @@ public class MainController {
 	}
 
 	@GetMapping(value = "/direction/{id}")
-	public void method(HttpServletResponse httpServletResponse,@PathVariable("id") long id) {
+	public void method(HttpServletResponse httpServletResponse, @PathVariable("id") long id) {
 		List<Parking_details> gui_parking_details = parking_cards();
 		for (Parking_details gui_parking_detail : gui_parking_details) {
-			if (gui_parking_detail.getParkingid()== id) {
+			if (gui_parking_detail.getParkingid() == id) {
 				System.out.println("Testing" + gui_parking_detail.getMaps());
 				httpServletResponse.setHeader("Location", gui_parking_detail.getMaps());
 				httpServletResponse.setStatus(302);
 			}
 		}
 	}
+
 	@GetMapping(value = "/payment/{id}")
 	public String payment(Model model, @PathVariable("id") String id) throws SQLException, ClassNotFoundException {
-		List<Parking_details> gui_parking_details = parking_cards();
 		ArrayList<booking_details> booking_detail_gui = payment_cards(Integer.valueOf(id));
 		for (booking_details booking_detail_guis : booking_detail_gui) {
-				System.out.println("Testing" + booking_detail_guis.getPlace());
-				model.addAttribute("p1", booking_detail_gui.get(0));
-				return "abc1";
+			System.out.println("Testing" + booking_detail_guis.getPlace());
+			model.addAttribute("p1", booking_detail_gui.get(0));
+			return "abc1";
 		}
 		return null;
 	}
+
 	@GetMapping(value = "/view/{id}")
 	public String view(Model model, @PathVariable("id") String id) throws SQLException, ClassNotFoundException {
 		List<Parking_details> gui_parking_details = parking_cards();
@@ -103,4 +107,23 @@ public class MainController {
 		return null;
 	}
 
+	@GetMapping("/admin")
+	public String admin(Model model) {
+		List<Parking_details> gui_parking_details = parking_cards();
+		model.addAttribute("p1", gui_parking_details);
+		for (Parking_details gui_parking_detail : gui_parking_details) {
+			if (email_123.equals("sparsh1176@gmail.com")) {
+				model.addAttribute("p1", gui_parking_details);
+				return "Admin_home";
+			}
+		}
+		return null;
+	}
+
+	@GetMapping(value = "/deletion/{id}")
+	public String deletion(Model model, @PathVariable("id") long id) {
+		List<admin_details> gui_parking_details = admin_cards((int) id);
+		return "404";
+	}
 }
+
